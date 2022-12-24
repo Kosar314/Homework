9ccee881523c004e2393aa746c20e2432e53ee43
Kosar <121202058+Kosar314@users.noreply.github.com>
2022-12-24 23:45:14 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework // Название пространства имён, у тебя будет своё
+{
+    internal class Program // Название класса, у тебя будет скорее всего такое же, но не факт
+    {
+        static void Main(string[] args)
+        {
+            QuickStart.Task10(); // Вызываешь нужные задачи
+
+            Console.WriteLine("Нажмите enter для завершения"); // Нужно чтобы не закрывалось окно после выполнения
+            Console.ReadLine();
+        }
+    }
+}
